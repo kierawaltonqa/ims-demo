8cfe9153940968d8b27254039f75ac8d97d7d13f
Kiera Walton <kwalton@qa.com>
2021-01-12 13:54:12 +0000

    added schema for creating the tables for the database to sql-schema.sql

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,4 +1,5 @@
 create database if not exists ims;
 create table if not exists ims.customers(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
-create table if not exists ims.items(item_id int primary key auto_increment, item_name varchar(40),item_price decimal(6,2));
-create table if not exists ims.orders(order_id int primary key auto_increment, customer_id int, item_id int, foreign key(customer_id) references customers(id), foreign key(item_id) references items(item_id));
+create table if not exists ims.items(itemID int primary key auto_increment, itemName varchar(40), itemPrice varchar(40));
+create table if not exists ims.orders(orderID int primary key auto_increment, customerID int, orderDate varchar(20) not null, foreign key(customerID) references ims.customers(id));
+create table if not exists ims.orderline(orderlineID int primary key auto_increment, itemID int, orderID int, foreign key(itemID) references ims.items(itemID), foreign key(orderID) references ims.orders(orderID));
