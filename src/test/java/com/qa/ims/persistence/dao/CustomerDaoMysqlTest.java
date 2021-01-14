2959f97fb9e8e1e2811ec6bc1702f458d237bd54
Kiera Walton <kwalton@qa.com>
2021-01-14 17:21:09 +0000

    fixed error

---
+++ src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
@@ -0,0 +1,18 @@
+package com.qa.ims.persistence.dao;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomerDaoMysqlTest {
+
+//	@Mock
+//	private String jdbcConnectionUrl;
+//	private String username;
+//	private String password;
+//	
+//	@Spy
+//	@InjectMocks
+//	private CustomerDaoMysql customerDaoMysql;
+
+}
